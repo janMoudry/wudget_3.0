f7f05111bda4a7b52a30e8fee9484edcaaa5151a
Jan Moudrý <97884823+janMoudry@users.noreply.github.com>
2025-05-25 18:32:21 +0200

    Create missing product pages

---
+++ src/pages/ProductCreate.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductCreate() {
+  return (
+    <div>
+      <h1>Create Product</h1>
+      {/* Add your product creation form here */}
+    </div>
+  );
+}
+
+export default ProductCreate;
---
+++ src/pages/ProductEdit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductEdit() {
+  return (
+    <div>
+      <h1>Edit Product</h1>
+      {/* Add your product editing form here */}
+    </div>
+  );
+}
+
+export default ProductEdit;
